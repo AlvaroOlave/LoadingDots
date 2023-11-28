9e31826b53791a7863efce72b4165b96240e5890
AlvaroOlave <alvaro.olave.baneres@gmail.com>
2023-11-28 16:28:37 +0100

    min ios version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LoadingDots",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v8)],
     products: [
         .library(
             name: "LoadingDots",
